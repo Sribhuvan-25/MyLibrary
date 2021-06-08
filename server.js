1eca0abc72b0ae0d7413e09c85e802e83d4f9975
Sribhuvan Reddy Yellu <sribhuvan.git@gmail.com>
2021-06-08 09:09:34 -0400

    Adding and searching authors

--- server.js
+++ server.js
@@ -1,26 +1,3 @@
-// const express = require('express')
-// const app  = express();
-// const expressLayout = require('express-ejs-layouts');
-// const mongoose = require('mongoose');
-
-
-// mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser:true, useUnifiedTopology: true })
-// const db = mongoose.connection;
-// db.on('error', (error)=>console.error(error));
-// db.once('open', ()=>console.log('Connected'));
-
-
-// app.set('view engine', 'ejs')
-// app.set('views', __dirname + '/views')
-// app.set('layout','layouts/layout')
-// app.use(expressLayout)
-// app.use(express.static('public'))
-
-// const indexRouter = require('./routes/index')
-
-// app.use('/',indexRouter)
-
-// app.listen(process.env.PORT || 3000)
 
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
@@ -31,6 +8,8 @@ if (process.env.NODE_ENV !== 'production') {
   const expressLayouts = require('express-ejs-layouts')
   const mongoose = require('mongoose')
   const indexRouter = require('./routes/index')
+  const authorRouter = require('./routes/authors')
+  const bodyParser = require('body-parser')
 
   mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   const db = mongoose.connection
@@ -42,8 +21,9 @@ if (process.env.NODE_ENV !== 'production') {
   app.set('layout', 'layouts/layout')
   app.use(expressLayouts)
   app.use(express.static('public'))
-  
+  app.use(bodyParser.urlencoded({limit:'10mb', extended:false}))
   
   app.use('/', indexRouter)
+  app.use('/authors',authorRouter)
   
   app.listen(process.env.PORT || 3000)
